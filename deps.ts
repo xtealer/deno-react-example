5d23027f0e49a01be8bf94e3bbde583d863364e2
Enrique Shunnar <xtealer00@gmail.com>
2020-08-01 07:55:36 -0500

    initial project files

---
+++ deps.ts
@@ -0,0 +1,4 @@
+export { default as React } from 'https://dev.jspm.io/react@16.13.1';
+
+export { default as ReactDOMServer } from 'https://dev.jspm.io/react-dom@16.13.1/server';
+export { opine } from 'https://deno.land/x/opine@0.19.1/mod.ts';
---
+++ server.tsx
@@ -0,0 +1,30 @@
+import {
+  opine,
+  React,
+  ReactDOMServer,
+} from "./deps.ts";
+
+import App from "./app.tsx";
+
+const app = opine();
+const browserBundlePath = "/browser.js";
+
+const js =
+  `import React from "https://dev.jspm.io/react@16.13.1";\nimport ReactDOM from "https://dev.jspm.io/react-dom@16.13.1";\nconst App = ${App};\nReactDOM.hydrate(React.createElement(App), document.body);`;
+
+const html =
+  `<html><head><script type="module" src="${browserBundlePath}"></script><style>* { font-family: Helvetica; }</style></head><body>${
+    (ReactDOMServer as any).renderToString(<App />)
+  }</body></html>`;
+
+app.use(browserBundlePath, (req, res, next) => {
+  res.type("application/javascript").send(js);
+});
+
+app.use("/", (req, res, next) => {
+  res.type("text/html").send(html);
+});
+
+app.listen({ port: 3000 });
+
+console.log("React SSR App listening on port 3000");
